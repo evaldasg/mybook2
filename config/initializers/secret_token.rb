992b8bf3e3212c65441c41c2085c5f3417e711af
manoevis <user_evis@server.fake>
2013-10-07 18:53:46 +0300

    created rails app, added status update with scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mybook2::Application.config.secret_key_base = '0cc4d9d9e96ad9415fc7a014d9e34077caf79569a89c46029e731f3b706bd2e5212a664cab6eebdd8b35bb26f876ffd5a401cf487b7883792e55ab7d8ec2ab45'
